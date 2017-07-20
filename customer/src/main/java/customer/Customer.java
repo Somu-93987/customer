569c66966b34d176f15c2919cac9b76c10bef4fe
SOMU93987 <yoursomu.91@gmail.com>
2017-07-20 12:14:10 +0530

    asdfadsfasfd

---
+++ customer/src/main/java/customer/Customer.java
@@ -0,0 +1,13 @@
+package customer;
+
+public class Customer {
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World1!" );
+        System.out.println( "Hello World!2" );
+        System.out.println( "Hello World!3" );
+        System.out.println( "Hello World!" );System.out.println( "Hello World!3" );
+        System.out.println( "Hello World!34344" );
+       
+    }
+}
